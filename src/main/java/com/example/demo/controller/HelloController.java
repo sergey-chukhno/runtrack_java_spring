2a3b75f4f0a12107f98c7ee02c5bd915ae8ea521
Sergey Chukhno <sergeychukhno@MacBook-Air-de-Sergey.local>
2025-09-02 16:05:03 +0200

    Added Jour01 of Spring Boot Application runtrack

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
+@GetMapping("/")
+    public String hello() {
+        return greetingMessage;
+}
+}
