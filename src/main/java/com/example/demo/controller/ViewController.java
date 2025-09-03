b339485391102121436c857ad2bdcf80da5f5e02
Sergey Chukhno <sergeychukhno@MacBook-Air-de-Sergey.local>
2025-09-03 13:32:17 +0200

    Added Job04 of Day02

--- src/main/java/com/example/demo/controller/ViewController.java
+++ src/main/java/com/example/demo/controller/ViewController.java
@@ -3,16 +3,23 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class ViewController {
 
-    @GetMapping("/messages")
-    public String sayMessage(Model model) {
-        List<String> messages = List.of("Hello", "How are you?", "Have a nice day!");
-        model.addAttribute("messages", messages);
+    @GetMapping("/welcome")
+    public String showForm() {
         return "view";
     }
+
+    @PostMapping("/welcome")
+    public String handleForm(@RequestParam("name") String name, Model model) {
+        model.addAttribute("message", "Bienvenue, " + name + " !");
+        return "view";
+    }
+
 }
