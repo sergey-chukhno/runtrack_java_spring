a2474a4c98d431377187e21cd368cd88ebbe9ebb
Sergey Chukhno <sergeychukhno@MacBook-Air-de-Sergey.local>
2025-09-05 00:22:29 +0200

    Added Job6 of Jour3

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -1,32 +1,44 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Person;
+import com.example.demo.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import com.example.demo.repository.PersonRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PersonController {
 
-    private final PersonRepository personRepository;
+    @Autowired
+    private PersonRepository personRepository;
 
-    public PersonController(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     @GetMapping("/persons")
-    public String listPersons(Model model) {
+    public String getPersons(Model model) {
         model.addAttribute("persons", personRepository.findAll());
         return "persons";
     }
 
 
-    @PostMapping("/persons")
-    public String addPerson(@RequestParam String name, @RequestParam int age) {
-        personRepository.save(new Person());
+    @GetMapping("/persons/delete/{id}")
+    public String deletePerson(@PathVariable Long id) {
+        personRepository.deleteById(id);
+        return "redirect:/persons";
+    }
+
+
+    @GetMapping("/persons/edit/{id}")
+    public String editPersonForm(@PathVariable Long id, Model model) {
+        Person person = personRepository.findById(id).orElseThrow();
+        model.addAttribute("person", person);
+        return "edit-person";
+    }
+
+
+    @PostMapping("/persons/update")
+    public String updatePerson(@ModelAttribute Person person) {
+        personRepository.save(person);
         return "redirect:/persons";
     }
 }
